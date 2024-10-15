c84b4cf35b03d7d859721bbef53089fef28b8b0a
Abdullah Abazza <7a7abas@gmail.com>
2024-10-15 07:22:34 +0300

    ADD Images

---
+++ index.js
@@ -0,0 +1,71 @@
+//Logo
+import logoWhite from "./logo/logo_white.png";
+import logoBlack from "./logo/logo_black.png";
+
+//Astro
+
+import astronaut from "./astronaut/astronaut_green.svg";
+
+//Tech
+
+import codecademy from "./tech/codecademy.png";
+import htmlIcon from "./tech/html.png";
+import cssIcon from "./tech/css.png";
+import jsIcon from "./tech/javascript.png";
+import reactIcon from "./tech/reactjs.png";
+import threeIcon from "./tech/threejs.svg";
+import tailwindIcon from "./tech/tailwind.png";
+import gitIcon from "./tech/git.png";
+import githubIcon from "./tech/github.png";
+import githubCardIcon from "./tech/github-card.png";
+import figmaIcon from "./tech/figma.png";
+import reduxIcon from "./tech/redux.png";
+import gsapIcon from "./tech/gsap.png";
+import shopifyIcon from "./tech/shopify.png";
+import framerIcon from "./tech/framer.png";
+import webflowIcon from "./tech/webflow.svg";
+
+//Projects
+import FashionFusion from "./projects/FashionFusion.jpeg";
+import adminDashboard from "./projects/adminDashboard.jpeg";
+import portfolio from "./projects/portfolio.PNG";
+import todosList from "./projects/todosList.jpeg";
+import special from "./projects/special.jpeg";
+import abazza from "./projects/abazza.jpeg";
+import login from "./projects/login.jpeg";
+import newEditor from "./projects/newEditor.jpeg";
+import oldEditor from "./projects/oldEditor.jpeg";
+import subwayImages from "./projects/subwayImages.jpg";
+import subwayCode from "./projects/subwayCode.jpg";
+import hangman from "./projects/hangman.jpeg";
+import memoryGame from "./projects/memoryGame.jpeg";
+import typingGame from "./projects/typingGame.jpeg";
+import Bondi from "./projects/bondi.jpeg";
+import ResDesign from "./projects/ResDesign.jpeg";
+import dashboard from "./projects/dashboard.jpeg";
+import kasper from "./projects/kasper.jpeg";
+import leon from "./projects/leon.jpeg";
+
+//Memoji
+
+import memojiHand from "./memoji/memoji_hand-min.png";
+import memojiHello from "./memoji/memoji_hello-min.png";
+import memojiOk from "./memoji/memoji_ok-min.png";
+import memojiMyPhoto from "./memoji/myPhoto-min.png";
+import memojiLinkedIn from "./memoji/linkedin-min.png";
+
+//Certificates
+import htmlCertificate from "./certificates/Html_Codecademy_Certificate.png";
+import cssCertificate from "./certificates/Css_Codecademy_Certificate.png";
+import jsCertificate from "./certificates/JavaScript_Codecademy_Certificate.png";
+import reactCertificate from "./certificates/React_Codecademy_Certificate.png";
+import webflowCertificate from "./certificates/Webflow_101_Certificate.png";
+
+import globeTexture from "./globe.svg";
+
+export {
+  astronaut, codecademy, htmlIcon, cssIcon, jsIcon, reactIcon, threeIcon, tailwindIcon, gitIcon, githubIcon, githubCardIcon, figmaIcon, reduxIcon,
+  gsapIcon, shopifyIcon, framerIcon, webflowIcon, memojiHand, memojiHello, memojiOk, htmlCertificate, cssCertificate, jsCertificate, reactCertificate, 
+  webflowCertificate, globeTexture, logoWhite, logoBlack, memojiMyPhoto, memojiLinkedIn, FashionFusion, adminDashboard, todosList, special, abazza, login, 
+  newEditor, oldEditor, subwayImages, subwayCode, hangman, memoryGame, typingGame, Bondi, ResDesign, dashboard, kasper, leon, portfolio
+};
